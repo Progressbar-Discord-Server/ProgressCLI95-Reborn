a57004203b13c8fbf89cae308660571f9a5ce4b3
Anananel <dmitrenko.ivan08@gmail.com>
2022-09-18 21:16:22 +0300

    BSOD with adaptive size

--- src/game/bsod.py
+++ src/game/bsod.py
@@ -4,12 +4,26 @@ def trigger(system: str='95') -> None:
 
     lang = language.Language()
 
-    rprint(f'[white on #00007f]                 [/white on #00007f][bold #00007f on white]   {lang.bsod.bsod_header}   [/bold #00007f on white][white on #00007f]                 [/white on #00007f]')
-    rprint(f'[white on #00007f]                                                 [/white on #00007f]')
-    rprint(f'[white on #00007f] {lang.bsod.bsod1}    [/white on #00007f]')
-    rprint(f'[white on #00007f] {lang.bsod.bsod2}     [/white on #00007f]')
-    rprint(f'[white on #00007f]                                                 [/white on #00007f]')
-    rprint(f'[white on #00007f] {lang.bsod.bsod3}  [/white on #00007f]')
-    rprint(f"[white on #00007f] {lang.bsod.bsod4}           [/white on #00007f]")
-    rprint(f'[white on #00007f]                                                 [/white on #00007f]')
+    # this is a mess
+    # TODO: Universal message screen printer
+
+    header = lang.bsod.bsod_header
+    lines = [lang.bsod['bsod' + str(x)] for x in range(1, 5)]
+    longest_len = max([len(x) for x in lines])
+    window_width = longest_len + 2
+
+    header_indent = (window_width - len(header)) // 2
+    if header_indent * 2 + len(header) < window_width:
+        window_width += 1
+        header_indent += 1
+        pass
+    rprint('[white on #00007f]' + ' ' * window_width + '[/]')
+    rprint('[bold white on #00007f]' + ' ' * header_indent + header + ' ' * header_indent + '[/]')
+    rprint('[white on #00007f]' + ' ' * window_width + '[/]')
+    rprint(f'[white on #00007f] {lang.bsod.bsod1}' + ' ' * (window_width - len(lang.bsod.bsod1) - 1) + '[/]')
+    rprint(f'[white on #00007f] {lang.bsod.bsod2}' + ' ' * (window_width - len(lang.bsod.bsod2) - 1) + '[/]')
+    rprint('[white on #00007f]' + ' ' * window_width + '[/]')
+    rprint(f'[white on #00007f] {lang.bsod.bsod3}' + ' ' * (window_width - len(lang.bsod.bsod3) - 1) + '[/]')
+    rprint(f'[white on #00007f] {lang.bsod.bsod4}' + ' ' * (window_width - len(lang.bsod.bsod4) - 1) + '[/]')
+    rprint('[white on #00007f]' + ' ' * window_width + '[/]')
     input()
